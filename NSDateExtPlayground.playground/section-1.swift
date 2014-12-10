980b1d8c59871a16ba5e16f903f9f925843254c7
Chris Wagner <cwagner118@gmail.com>
2014-12-10 13:16:31 -0700

    Update playgroud

--- NSDateExtPlayground.playground/section-1.swift
+++ NSDateExtPlayground.playground/section-1.swift
@@ -4,7 +4,19 @@ import NSDateExt
 
 
 NSDate.today().midnight
-NSDate.tomorrow().midnight
+NSDate.tomorrow().midnight.shortDateString
 NSDate.yesterday().endOfDay
 
-NSDate().dateByAdding(.Years(-4))
+NSDate().dateByAdding(.Years(-4)).shortDateString
+
+
+NSDate().dateByAdding(units: TimeUnit.Days(1), TimeUnit.Hours(5))
+
+NSDate.today() + .Days(5)
+NSDate.today().midnight + 2.hours + 30.minutes + 5.seconds
+
+NSDate.today().dateByAdding(units: 2.hours, 30.minutes)
+
+let yesterday = NSDate.yesterday().mediumDateString
+
+
