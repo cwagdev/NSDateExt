6345cf2aa5c783b96e5cff7d9c6533f46c5ccf6a
Chris Wagner <cwagner118@gmail.com>
2014-12-10 11:47:48 -0700

    Create Xcode Workspace

---
+++ NSDateExt/NSDateExt.h
@@ -0,0 +1,19 @@
+//
+//  NSDateExt.h
+//  NSDateExt
+//
+//  Created by Chris Wagner on 12/10/14.
+//  Copyright (c) 2014 Chris Wagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSDateExt.
+FOUNDATION_EXPORT double NSDateExtVersionNumber;
+
+//! Project version string for NSDateExt.
+FOUNDATION_EXPORT const unsigned char NSDateExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSDateExt/PublicHeader.h>
+
+
